1200465cef5b02e9a5579a4d87d7c79129e6ba9e
Rodolfo Baez Jr <rbjengineeringservices.llc@gmail.com>
2025-06-22 00:34:54 -0500

    Updated website endpoint and digital business card endpoint

--- qrgenerator.py
+++ qrgenerator.py
@@ -1,5 +1,5 @@
 import qrcode
-ip_address = "one-repo-to-rule-them-all.github.io/rbj-digital-business-card/"  # Replace with your actual local IP
-url = f"http://{ip_address}"
+ip_address = "one-repo-to-rule-them-all.github.io/rbj-engineering-digital-business-card/"  # Replace with your actual local IP
+url = f"https://{ip_address}"
 img = qrcode.make(url)
 img.save("business_card_qr.png")
